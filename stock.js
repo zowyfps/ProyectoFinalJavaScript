ade6dec6c8ef2952f94042b4d41961ff1bb2b958
zowyfps <1rociozoe@gmail.com>
2022-07-31 22:08:52 -0300

    ProyectoFinalJava

---
+++ stock.js
@@ -0,0 +1,8 @@
+let stockProductos = [
+    {id: 1, freeshipping: false, nombre: "DAX J", tipo: "EVENTO", desc: "9/7/2022 OTC", precio: 2000,  img: './img/dax.png'},
+    {id: 2, freeshipping: true, nombre: "I HATE MODELS", tipo: "EVENTO", desc: "16/7/2022 OTC", precio: 2000,  img: './img/ihate.png'},
+    {id: 3, freeshipping: false, nombre: "MARIANO DC", tipo: "EVENTO", desc: "23/7/2022 OTC", precio: 2000, img: './img/mariano.png'},
+    {id: 4, freeshipping: false, nombre: "Dr RUBENSTEIN", tipo: "EVENTO", desc: "30/7/2022 OTC", precio: 2000,  img: './img/rubi.png'},
+    {id: 5, freeshipping: false, nombre: "OBSCURE SHAPE&SHADOW", tipo: "EVENTO", desc: "9/7/2022 OTC", precio: 2000,  img: './img/6.png'},
+
+]   
